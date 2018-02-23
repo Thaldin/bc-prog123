1e4b05afe0da5851e08bae9b13a12bff9f45c7a5
Ed Abshire <thaldin@comcast.net>
2018-02-22 19:05:07 -0800

    Phase 6

---
+++ Abshire_Ed/Controllers/AbshireController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abshire_Ed.Controllers
+{
+    public class AbshireController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
